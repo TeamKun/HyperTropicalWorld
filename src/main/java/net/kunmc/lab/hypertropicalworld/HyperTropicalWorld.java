fe96bf218f5eed5892a25783fefdf2290e30925d
maru-256 <maru043@docomo.ne.jp>
2021-08-26 05:23:34 +0900

    熱中症によるダメージ処理を追加

--- src/main/java/net/kunmc/lab/hypertropicalworld/HyperTropicalWorld.java
+++ src/main/java/net/kunmc/lab/hypertropicalworld/HyperTropicalWorld.java
@@ -1,5 +1,7 @@
 package net.kunmc.lab.hypertropicalworld;
 
+import net.kunmc.lab.hypertropicalworld.task.DamageTask;
+import net.kunmc.lab.hypertropicalworld.task.IncrementHeatstrokeLevelTask;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HyperTropicalWorld extends JavaPlugin {
@@ -8,6 +10,9 @@ public final class HyperTropicalWorld extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+
+        new IncrementHeatstrokeLevelTask().runTaskTimer(this, 0, 20);
+        new DamageTask().runTaskTimer(this, 0, 20);
     }
 
     @Override
--- src/main/java/net/kunmc/lab/hypertropicalworld/task/DamageTask.java
+++ src/main/java/net/kunmc/lab/hypertropicalworld/task/DamageTask.java
@@ -1,2 +1,19 @@
-package net.kunmc.lab.hypertropicalworld.task;public class DamageTask {
+package net.kunmc.lab.hypertropicalworld.task;
+
+import net.kunmc.lab.hypertropicalworld.util.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class DamageTask extends BukkitRunnable {
+    public static int heatstrokeLevelBorder = 30;
+    public static double damage = 1.0;
+
+    @Override
+    public void run() {
+        Bukkit.getOnlinePlayers().forEach(p -> {
+            if (Utils.getHeatstrokeLevel(p) >= heatstrokeLevelBorder) {
+                p.damage(damage);
+            }
+        });
+    }
 }
