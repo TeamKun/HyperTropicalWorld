7f32a39da9b226bab7ad29c53df20faeac1397be
maru-256 <maru043@docomo.ne.jp>
2021-08-28 06:53:27 +0900

    水ボトル汲み取りの同期ズレを解消

--- src/main/java/net/kunmc/lab/hypertropicalworld/listener/BottleFilledListener.java
+++ src/main/java/net/kunmc/lab/hypertropicalworld/listener/BottleFilledListener.java
@@ -7,9 +7,16 @@ import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Levelled;
 import org.bukkit.block.data.Waterlogged;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionType;
 import org.jetbrains.annotations.NotNull;
 
 public class BottleFilledListener implements ListenerAction<PlayerInteractEvent> {
@@ -24,8 +31,9 @@ public class BottleFilledListener implements ListenerAction<PlayerInteractEvent>
             return;
         }
 
-        Block block = e.getPlayer().getTargetBlock(5, TargetBlockInfo.FluidMode.SOURCE_ONLY);
-        if (block == null) {
+        Player p = e.getPlayer();
+        Block block = p.getTargetBlock(5, TargetBlockInfo.FluidMode.SOURCE_ONLY);
+        if (block == null || !block.getType().equals(Material.WATER)) {
             return;
         }
         BlockData data = block.getBlockData();
@@ -50,5 +58,21 @@ public class BottleFilledListener implements ListenerAction<PlayerInteractEvent>
                 block.setBlockData(waterlogged);
             }
         }
+
+        item.setAmount(item.getAmount() - 1);
+
+        ItemStack bottle = new ItemStack(Material.POTION);
+        bottle.setAmount(1);
+        PotionMeta meta = ((PotionMeta) bottle.getItemMeta());
+        meta.setBasePotionData(new PotionData(PotionType.WATER));
+        bottle.setItemMeta(meta);
+        p.getInventory().addItem(bottle).forEach((k, v) -> {
+            p.getWorld().spawnEntity(p.getEyeLocation(), EntityType.DROPPED_ITEM, CreatureSpawnEvent.SpawnReason.CUSTOM, x -> {
+                ((Item) x).setItemStack(v);
+            });
+        });
+
+
+        e.setCancelled(true);
     }
 }
