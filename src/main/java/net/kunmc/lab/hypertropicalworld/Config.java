8695b55f2594b6bc72fd0037329ff5d41fdfd3f9
maru-256 <maru043@docomo.ne.jp>
2021-08-28 07:08:14 +0900

    shouldShowLevelをデフォルトtrueに変更

--- src/main/java/net/kunmc/lab/hypertropicalworld/Config.java
+++ src/main/java/net/kunmc/lab/hypertropicalworld/Config.java
@@ -3,7 +3,7 @@ package net.kunmc.lab.hypertropicalworld;
 public class Config {
     public static int heatstrokeLevelBorder = 30;
     public static boolean isEnabled = false;
-    public static boolean shouldShowLevel = false;
+    public static boolean shouldShowLevel = true;
     public static boolean prohibitInfiniteWaterSource = false;
     public static double damage = 1.0;
     public static int slownessLevel = 0;
